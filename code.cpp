8b3be5ea867e295d6194348cbfe59b5ccdee08ff
SehwarMaryam <maryam.dev93@gmail.com>
2024-10-29 05:27:08 -0700

    chock

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    char grade = 'a';
+    float PI = 3.14F;
+
+    bool isSafe = true;
+    double price = 100.99;
+
+    cout << isSafe << endl; //true -> 1 & false -> 0
+    return 0;  
+}
+git config --global user.email "maryam.dev93@gmail.com"
+  git config --global user.name "SehwarMaryam"
