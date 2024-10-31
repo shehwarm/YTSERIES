cc5324b78748d5d66aaa928d7b4140bf446c3fb4
SehwarMaryam <maryam.dev93@gmail.com>
2024-10-31 04:22:15 -0700

    runned a program

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cin >> marks;
+    if (marks >= 100)
+    {
+        cout << "A/n";
+    }
+    else
+    {
+        cout << "B/n";
+    }
+    return 0;
+}
