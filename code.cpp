3e23446d41c955b1cd3c66d043f80ef4df652572
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-01 10:02:43 -0700

    chock

--- code.cpp
+++ code.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int marks;
-    cin >> marks;
-    if (marks >= 100)
-    {
-        cout << "A/n";
-    }
-    else
-    {
-        cout << "B/n";
-    }
+int main() {
+   int n = 7;
+   bool isPrime = true;
+
+   for(int i=2; i<=n-1; i++) {
+       if(n % i == 0) { //non prime
+           isPrime = false;
+           break;
+
+       }
+   }
+    
     return 0;
-}
+} 
