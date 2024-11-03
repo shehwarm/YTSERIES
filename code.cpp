8524a67bfb8e3d158a975c657aa51bdb318128d1
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-03 08:26:46 -0800

    product of two numbers

--- code.cpp
+++ code.cpp
@@ -5,9 +5,9 @@ int main() {
    int num1 = 7;
    int num2 = 6;
     
-   int sum = num1 + num2; 
+   int product = num1 * num2; 
 
-   cout << sum << endl;
+   cout << product << endl;
    
     
     return 0;
