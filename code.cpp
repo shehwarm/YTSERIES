844388b9bc3d841ec7d025e15e8fa254ac302cde
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-03 08:29:34 -0800

    divide two numbers

--- code.cpp
+++ code.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-   int num1 = 7;
+   int num1 = 12;
    int num2 = 6;
     
-   int product = num1 * num2; 
+   int divide = num1 / num2; 
 
-   cout << product << endl;
+   cout << divide << endl;
    
     
     return 0;
