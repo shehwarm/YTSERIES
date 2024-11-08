ae6665481e7e4c1433ffa3143d6168271e7957ed
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-08 03:31:26 -0800

    used nested loops

--- code.cpp
+++ code.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main() {
-    int n = 45;
-
-    if(n >= 0)  {
-        cout << "n is positive\n";
+    int n = 10;
+    for(int i=1; i<=n; i++){
+        int m = 5;
+        for(int i=1; i<=m; i++) {
+            cout << "*";
+        }
+        cout << endl;
     }
-
-    return 0;  
 }
