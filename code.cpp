f728b375808324587f4a4d1694fdd336f9030b60
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-10 09:16:54 -0800

    floating point numbers

--- code.cpp
+++ code.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
-using namespace std;
+using namespace std;     
+
 int main() {
-    for (int num = 2; num <= 100; num++) {
-        bool isPrime = true;  
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                isPrime = false;  
-                break;
-            }
-        }  
-        if (isPrime) {
-            cout << num << " ";
-        }
-    }
-    cout << endl; 
+    int num1, num2;
+    float num;
+   
+    cout << "Enter first integers: ";
+    cin >> num1;
+    cout<< "Enter second integers: ";
+    cin>> num2;
+    cout << "Enter a floating-point number: ";
+    cin >> num;
+    
+
+    float sum = num1 + num2;
+    float sumAsfloat= (float)sum;
+    
+    float product = num1 * num2; 
+       float productAsfloat= (float)product;
+    
+    
+    int division = (int)(num / sum); 
+
+     cout << "Sum as float: " << sumAsfloat << endl;
+         cout << "Product as float: " << productAsfloat << endl;
+             cout << "Division result as int: " << division << endl;
     return 0;
 }
