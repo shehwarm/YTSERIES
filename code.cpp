c86e840b41efd8adb8a6d95ebd8b27c0284cba52
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-14 07:43:56 -0800

    used nested loops

--- code.cpp
+++ code.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int number, rev = 0;
-    cout << "Enter a number: ";
-    cin >> number;
-    do {
-        rev = rev * 10 + (number % 10);  
-        number = number / 10;  
-    } while (number != 0);  
-    cout << "Reversed number: " << rev << endl;
+    int n = 25;
+    int count = 1;
+    
+    for (int i = 1; i <= 5; i++) { 
+        for (int j = 1; j <= 5 - i; j++) {
+            cout << "  "; 
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl; 
+    }
+
+    count = 16;
+    for (int i = 4; i >= 1; i--) { 
+        for (int j = 1; j <= 5 - i; j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl; 
+    }
+
     return 0;
 }
