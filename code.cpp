d9ad9c38302c94e932e990de7908c45eaa348f61
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-29 19:07:58 +0500

    sizeof(marks)

--- code.cpp
+++ code.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main() {
    int marks[5] = {99,100,54,36,88};
-   marks[0] = 234;
-   cout << marks[0] << endl;
-   cout << marks[1] << endl;
-   cout << marks[2] << endl;
-   cout << marks[3] << endl;
-   cout << marks[4] << endl;
+   int size = 5;
+  cout << sizeof(marks) << endl;
 
    return 0;
 }
