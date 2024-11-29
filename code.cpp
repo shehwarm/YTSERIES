a1356137cd2c7d1ece321acea6cfcfc292ff1d15
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-29 19:23:31 +0500

    smallest and largest in array

--- code.cpp
+++ code.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main() {
-   int marks[5] = {99,100,54,36,88};
-   int size = 5;
-  cout << sizeof(marks) << endl;
+    int num[] = {5, 15, 22, 1, -15, 24};
+   int size = 6;
+   int largest = INT_MIN;
 
+   for(int i=0; i < size; i++){
+    if (num[i] > largest) {
+        largest = num[i];
+    }
+   }
+  cout << "largest: " << largest << endl;
+ 
    return 0;
 }
