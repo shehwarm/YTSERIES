43ccb9ca9909d24c8d2933aee881a0b32eebd2c5
SehwarMaryam <maryam.dev93@gmail.com>
2024-11-29 19:00:55 +0500

    arrays

--- code.cpp
+++ code.cpp
@@ -2,31 +2,13 @@
 using namespace std;
 
 int main() {
-    int n = 25;
-    int count = 1;
-    
-    for (int i = 1; i <= 5; i++) { 
-        for (int j = 1; j <= 5 - i; j++) {
-            cout << "  "; 
-        }
-        for (int j = 1; j <= i; j++) {
-            cout << count << " ";
-            count++;
-        }
-        cout << endl; 
-    }
+   int marks[5] = {99,100,54,36,88};
+   marks[0] = 234;
+   cout << marks[0] << endl;
+   cout << marks[1] << endl;
+   cout << marks[2] << endl;
+   cout << marks[3] << endl;
+   cout << marks[4] << endl;
 
-    count = 16;
-    for (int i = 4; i >= 1; i--) { 
-        for (int j = 1; j <= 5 - i; j++) {
-            cout << "  ";
-        }
-        for (int j = 1; j <= i; j++) {
-            cout << count << " ";
-            count++;
-        }
-        cout << endl; 
-    }
-
-    return 0;
+   return 0;
 }
