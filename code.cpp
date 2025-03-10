9fa047c2f2f9e97ac2ea4e993d1acc9bfecb7eaa
SehwarMaryam <maryam.dev93@gmail.com>
2025-03-10 16:36:56 +0500

    Refactor Employee class to Car class with accelerate method and update main function

--- code.cpp
+++ code.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
 
-class Employee{
-    public:
-      string name;
-      string company;
-      int age;
+// Class definition
+class Car {
+public:    // Access specifier
+    // Data members (attributes)
+    string brand;
+    int year;
+    float speed;
 
-    void IntroduceYourself(){
-        cout<<" Name = "<<name<<endl;
-        cout<<" Company = "<<company<<endl;
-        cout<<" Age = "<<age<<endl;
-    }  
+    // Member function (method)
+    void accelerate() {
+        speed += 10;
+        cout << "Speed increased to " << speed << " km/h\n";
+    }
 };
 
-int main(){
-    Employee employee1;
-    employee1.name = "Maryam";
-    employee1.company = "Google";
-    employee1.age = 22;
-    employee1.IntroduceYourself();
+int main() {
+    // Creating objects of Car class
+    Car car1;  // Object 1
+    Car car2;  // Object 2
 
-    Employee employee2;
-    employee2.name = "Shehwar";
-    employee2.company = "Netflix";
-    employee2.age = 23;
-    employee2.IntroduceYourself();
+    // Accessing and setting object properties
+    car1.brand = "Toyota";
+    car1.year = 2020;
+    car1.speed = 0;
 
+    car2.brand = "Ford";
+    car2.year = 2022;
+    car2.speed = 50;
+
+    // Using object methods and properties
+    cout << car1.brand << " " << car1.year << endl;
+    car1.accelerate();
+
+    cout << car2.brand << " " << car2.year << endl;
+    car2.accelerate();
+
+    return 0;
 }
