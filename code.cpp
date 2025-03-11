a8c194c6cf00e1e5eff80d8cded8d5e1922e7ad2
SehwarMaryam <maryam.dev93@gmail.com>
2025-03-11 11:25:34 +0500

    Refactor Employee class to include constructor and getInfo method, update main function for instantiation

--- code.cpp
+++ code.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
 
 class Employee{
     public:
-      Employee(){
-            color="yellow";
+      Employee(string n, string c){
+            name = n;
+            company = c;
       }
+
       string name;
       string company;
-      int age;
-      string color;
+      
+      
 
-    void IntroduceYourself(){
+    void getInfo(){
         cout<<" Name = "<<name<<endl;
         cout<<" Company = "<<company<<endl;
-        cout<<" Age = "<<age<<endl;
+        
        
     }  
 };
 
 int main(){
-    Employee employee1;
-    employee1.name = "Maryam";
-    employee1.company = "Google";
-    employee1.age = 22;
-    employee1.IntroduceYourself();
+    Employee employee1("Maryam", "Amazon");
+    employee1.getInfo();
 
-    Employee employee2;
-    employee2.name = "Shehwar";
-    employee2.company = "Netflix";
-    employee2.age = 23;
-    employee2.IntroduceYourself();
-    cout<<employee1.color<<endl;
-    cout<<employee2.color<<endl;
-    return 0;
-
+   return 0;
 }
