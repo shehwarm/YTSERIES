e4a21af83bda46339571f80705c5493e4395aa7d
SehwarMaryam <maryam.dev93@gmail.com>
2025-03-18 16:47:53 +0500

    Refactor Student class to GradStudent, update constructors, and enhance getInfo method

--- code.cpp
+++ code.cpp
@@ -7,8 +7,9 @@ public:
     string name;
     int age;
 
-    Person() {
-       cout<<"Person Constructor"<<endl;
+    Person(string n, int a) {
+        name = n;
+        age = a;
     }
 };
 
@@ -16,22 +17,33 @@ public:
     public:
        int rollno;
 
-       Student() {
-              cout<<"Student Constructor"<<endl;
-        }
-    
+       Student(string n, int a, int r) : Person(n, a) {
+        rollno = r; }
+       };
 
-       void getInfo() {
-        cout << "Name = " << name << endl;
-        cout << "Age = " << age << endl;
-        cout << "Roll No = " << rollno << endl;
-    }
+    
+    class GradStudent : public Student {
+        public:
+             string researchArea;
+        
+           GradStudent(string n, int a, int r, string ra) : Student(n, a, r) {
+              researchArea = ra;
+           }
+        
+             void getInfo() {
+              cout << "Name = " << name << endl;
+              cout << "Age = " << age << endl;
+              cout << "Roll No = " << rollno << endl;
+              cout << "Research Area = " << researchArea << endl;
+         }
        
  };
     
 int main() {
-    Student s1("Amna", 22, 21345);
+    GradStudent s1("Amna", 22, 21345, "Machine Learning");
     
     s1.getInfo();
     return 0; 
 }
+
+
