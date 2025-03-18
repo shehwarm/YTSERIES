eee7484b7ab6480f690497b5b8ca55158d80f8aa
SehwarMaryam <maryam.dev93@gmail.com>
2025-03-18 18:50:35 +0500

    Implement GradStudent and TA classes  using hybrid inheritance

--- code.cpp
+++ code.cpp
@@ -17,39 +17,58 @@ class Student : public Person {
     Student(string n, int a , int r) : Person (n,r) {
         rollno = r;
     }
-    void getInfo() {
-        cout << "Name = " << name << endl;
-        cout << "Age = " << age << endl;
-        cout << "Roll No = " <<rollno<<endl;
-    }
+    
+};
+class GradStudent : public Student {
+    public:
+         string researchArea;
+    
+       GradStudent(string n, int a, int r, string ra) : Student(n, a, r) {
+          researchArea = ra;
+       }
+    
+         void getInfo() {
+          cout << "Name = " << name << endl;
+          cout << "Age = " << age << endl;
+          cout << "Roll No = " << rollno << endl;
+          cout << "= " << researchArea << endl;
+     }
+   
 };
 
-class Teacher : public Person {
+class Teacher{
     public:
     string subject;
     double salary;
-    Teacher(string n, int a, string s, double sal) : Person(n,a) {
+    Teacher(string s, double sal){
         subject = s;
         salary = sal;
     }
+
+};
+ 
+class TA : public Student, public Teacher {
+    public:
+    string researchArea;
+    TA(string n, int a, int r, string ra, string s, double sal) : Student(n, a, r), Teacher(s, sal) {
+        researchArea = ra;
+    }
     void getInfo() {
         cout << "Name = " << name << endl;
-        cout << "Age = " << age<<endl;
-        cout<< "Subject = " << subject<<endl;
-        cout << "Salary = " << salary<<endl;
-}
-
-
-       
- };
-    
+        cout << "Age = " << age << endl;
+        cout << "Roll No = " << rollno << endl;
+        cout << "Research Area = " << researchArea << endl;
+        cout << "Subject = " << subject << endl;
+        cout << "Salary = " << salary << endl;
+    }
+};
 int main() {
-    Student s1("Ali",20,2345);
-    s1.getInfo();
+GradStudent s1("Amna", 22, 21345, "Machine Learning");
+s1.getInfo();
 
-    Teacher t1("Amna", 22, "Machine Learning",22000);
-    t1.getInfo();
-    return 0; 
+TA t1("Ali", 25, 21346, "Data Science", "AI", 50000);
+t1.getInfo();
+
+return 0; 
 }
-
 
