74efce67ff9899c0d062e8aafa88de9506a53c9d
SehwarMaryam <maryam.dev93@gmail.com>
2025-03-19 20:11:13 +0500

    Refactor MathOperations class to Complex class; implement operator overloading for addition and add display method for complex numbers

--- code.cpp
+++ code.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class MathOperations {
+class Complex {
 public:
-    int add(int a, int b) { // Two parameters
-        return a + b;
+    int real, imag;
+
+    Complex(int r, int i) : real(r), imag(i) {}
+
+    // Overloading the '+' operator
+    Complex operator+(const Complex& obj) {
+        return Complex(real + obj.real, imag + obj.imag);
     }
-    
-    int add(int a, int b, int c) { // Three parameters
-        return a + b + c;
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
     }
 };
 
 int main() {
-    MathOperations math;
-    cout << math.add(2, 3) << endl;       // Output: 5
-    cout << math.add(2, 3, 4) << endl;    // Output: 9
+    Complex c1(3, 4), c2(1, 2);
+    Complex c3 = c1 + c2; // Calls overloaded + operator
+    c3.display();         // Output: 4 + 6i
     return 0;
 }
