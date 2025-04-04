f62a8892a79528d0c71613cd3aef26326ffe1661
SehwarMaryam <maryam.dev93@gmail.com>
2025-04-04 16:37:13 +0500

    Refactor SubmarineSandwich class to EvenNumber; implement even number management functionalities including value adjustment and retrieval

--- code.cpp
+++ code.cpp
@@ -1,89 +1,57 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-class SubmarineSandwich {
+class EvenNumber {
 private:
-    string breadType; // Type of bread (e.g., Italian, Whole Wheat, etc.)
-    vector<string> fillings; // List of fillings (e.g., Mortadella, Turkey, etc.)
-    vector<string> cheeses; // List of cheeses (e.g., Cheddar, Swiss, etc.)
-    vector<string> sauces; // List of sauces (e.g., Mayo, Mustard, etc.)
-    vector<string> vegetables; // List of vegetables (e.g., Lettuce, Tomato, etc.)
-    bool toasted; // Whether the sandwich is toasted or not
+    int value;
 
 public:
-    // Constructor
-    SubmarineSandwich(string bread, bool isToasted = false)
-        : breadType(bread), toasted(isToasted) {}
-
-    // Function to add a filling
-    void addFilling(const string& filling) {
-        fillings.push_back(filling);
+    EvenNumber() {
+        value = 0;
     }
 
-    // Function to add cheese
-    void addCheese(const string& cheese) {
-        cheeses.push_back(cheese);
+    void setValue(int val) {
+        if (val % 2 != 0) {
+            cout <<  << val << " is not even. Adjusting to " << (val + 1) << endl;
+            value = val + 1;
+        } else {
+            value = val;
+        }
     }
 
-    // Function to add a sauce
-    void addSauce(const string& sauce) {
-        sauces.push_back(sauce);
+    int getValue() const {
+        return value;
     }
 
-    // Function to add a vegetable
-    void addVegetable(const string& vegetable) {
-        vegetables.push_back(vegetable);
+    EvenNumber getNext() const {
+        EvenNumber next;
+        next.setValue(value + 2);
+        return next;
     }
 
-    // Function to display the sandwich details
-    void displaySandwich() const {
-        cout << "Submarine Sandwich Details:\n";
-        cout << "Bread Type: " << breadType << "\n";
-        cout << "Toasted: " << (toasted ? "Yes" : "No") << "\n";
-        
-        cout << "Fillings: ";
-        for (const auto& filling : fillings) {
-            cout << filling << " ";
-        }
-        cout << "\n";
-
-        cout << "Cheeses: ";
-        for (const auto& cheese : cheeses) {
-            cout << cheese << " ";
-        }
-        cout << "\n";
-
-        cout << "Sauces: ";
-        for (const auto& sauce : sauces) {
-            cout << sauce << " ";
-        }
-        cout << "\n";
-
-        cout << "Vegetables: ";
-        for (const auto& vegetable : vegetables) {
-            cout << vegetable << " ";
-        }
-        cout << "\n";
+    EvenNumber getPrevious() const {
+        EvenNumber prev;
+        prev.setValue(value - 2);
+        return prev;
     }
 };
 
 int main() {
-    // Create a submarine sandwich
-    SubmarineSandwich mySandwich("Italian", true);
-    mySandwich.addFilling("Mortadella");
-    mySandwich.addFilling("Turkey");
-    mySandwich.addCheese("Provolone");
-    mySandwich.addSauce("Mayo");
-    mySandwich.addSauce("Mustard");
-    mySandwich.addVegetable("Lettuce");
-    mySandwich.addVegetable("Tomato");
-    mySandwich.addVegetable("Onion");
+    EvenNumber num1;  
+    cout << "Initial even number: " << num1.getValue() << endl;
 
-    // Display the sandwich details
-    mySandwich.displaySandwich();
+    num1.setValue(4);
+    cout << "Current even number: " << num1.getValue() << endl;
+
+    EvenNumber next = num1.getNext();
+    cout << "Next even number: " << next.getValue() << endl;
+
+    EvenNumber prev = num1.getPrevious();
+    cout << "Previous even number: " << prev.getValue() << endl;
+
+    num1.setValue(7); 
+    cout << "Adjusted even number: " << num1.getValue() << endl;
 
     return 0;
 }
+
