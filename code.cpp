0589d6b82edbbefc9991676b2417c3ed6382f91f
SehwarMaryam <shehwarm93@gmail.com>
2025-05-28 10:41:54 +0500

    Refactor Point class to Library; implement library name management and book association functionalities

--- code.cpp
+++ code.cpp
@@ -1,42 +1,36 @@
 #include <iostream>
-#include <math.h>
 #include <string>
 using namespace std;
 
-class Point {
+class Library {
 private:
-    int x;
-    int y;
-
+    string libraryName;
 public:
-    void setX(int newX) { x = newX; }
-    void setY(int newY) { y = newY; }
-
-    int getX() const { return x; }
-    int getY() const { return y; }
-
-    string print() const {
-        return "(" + to_string(x) + ", " + to_string(y) + ")";
+    Library(string name) : libraryName(name) {}
+    string getLibraryName() {
+        return libraryName;
     }
+};
 
-    double distanceTo(const Point& other) const {
-        int xDiff = x - other.x;
-        int yDiff = y - other.y;
-        return sqrt(pow(xDiff, 2) + pow(yDiff, 2));
+class Book {
+private:
+    string title;
+    Library* library; 
+public:
+    Book(string t, Library* lib) : title(t), library(lib) {}
+    void display() {
+        cout << "Book Title: " << title << ", Library: " << library->getLibraryName() << endl;
     }
 };
 
 int main() {
-    Point p1, p2;
-    p1.setX(5);
-    p1.setY(7);
-    p2.setX(3);
-    p2.setY(9);
+    Library lib("City Central Library");
 
-    cout << "Point 1: " << p1.print() << endl;
-    cout << "Point 2: " << p2.print() << endl;
+    Book b1("C++ Programming", &lib);
+    Book b2("Data Structures", &lib);
 
-    cout << "Distance between points: " << p1.distanceTo(p2) << endl;
+    b1.display();
+    b2.display();
 
     return 0;
 }
